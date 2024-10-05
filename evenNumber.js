443f1ad675c2364602a3e2524a32e75abaac34d8
Garab <125604165+Garab02@users.noreply.github.com>
2024-10-05 10:18:27 +0200

    Create evenNumber.js

---
+++ evenNumber.js
@@ -0,0 +1,15 @@
+// This is get even numbers from 1 to 100
+
+counter = 0;
+var num=[];
+
+for(var i = 0; i<=100; i++){
+  if(i%2==0){
+   
+    num[counter] = i;
+    counter++
+   
+   
+  }
+  }
+console.log(num);
